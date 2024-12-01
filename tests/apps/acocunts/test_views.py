ecdfda33608960031954e936ed72fa8f21407b0b
Youssef <Y.Ehab2289@nu.edu.eg>
2024-12-01 22:28:51 +0200

    test: added register tests

--- tests/apps/acocunts/test_views.py
+++ tests/apps/acocunts/test_views.py
@@ -1,7 +1,7 @@
 import pytest
 from rest_framework.response import Response
 from rest_framework.test import APIClient
-
+from rest_framework import status
 from quickwrench_api.apps.accounts.models import Account
 
 
@@ -36,3 +36,22 @@ class TestLoginView:
 
         assert "access" in response.data
         assert "refresh" in response.data
+
+
+class TestRegister:
+    @pytest.mark.django_db
+    def test_register_success_201(self, ac, user_data):
+        response = ac.post("/accounts/register/", user_data, format="json")
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["username"] == user_data["username"]
+        assert response.data["email"] == user_data["email"]
+
+    @pytest.mark.django_db
+    def test_register_with_existing_email(
+        self, ac, user_data, user_with_existing_email
+    ):
+        user_data["email"] = user_with_existing_email.email
+        response = ac.post("/accounts/register/", user_data, format="json")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert "email" in response.data
+        assert response.data["email"] == ["An account with this email already exists."]
