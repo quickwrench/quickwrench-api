889927599889a65260dde5de7e5ab72fe2bdec47
Mo-UNO <mohamedamir4491@gmail.com>
2024-12-09 21:42:11 +0200

    feat: create serializers for workshop, service and category

---
+++ src/quickwrench_api/apps/workshops/serializers.py
@@ -0,0 +1,53 @@
+from typing import Iterable
+
+from rest_framework import serializers
+
+from ..accounts.serializers import AccountSerializer
+from .models import Account, Category, Workshop, Service
+
+
+class CategorySerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields: Iterable[str] = (
+            "name",
+            "description",
+        )
+
+
+class ServiceSerializers(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
+
+    class Meta:
+        model = Service
+        fields: Iterable[str] = (
+            "category",
+            "name",
+            "description",
+            "price",
+        )
+
+
+class WorkshopSerializers(serializers.ModelSerializer):
+    account: AccountSerializer = AccountSerializer()
+    services: serializers.PrimaryKeyRelatedField = serializers.PrimaryKeyRelatedField(
+        queryset=Service.objects.all(),
+        many=True,
+    )
+
+    class Meta:
+        model = Workshop
+        fields: Iterable[str] = (
+            "account",
+            "address",
+            "services",
+        )
+
+    def create(self, validated_data: dict) -> Workshop:
+        account_data: dict[str, str] = validated_data.pop("account")
+        services = validated_data.pop("services")
+        account: Account = Account.objects.create_user(**account_data)
+        validated_data["account"] = account
+        workshop: Workshop = Workshop.objects.create(**validated_data)
+        workshop.services.set(services)
+        return workshop
