1cf32bdabebb0f1ffefe868c56fa85f09c2ba8aa
Mo-UNO <mohamedamir4491@gmail.com>
2024-12-17 01:07:56 +0200

    feat: create category api view

--- src/quickwrench_api/apps/workshops/views.py
+++ src/quickwrench_api/apps/workshops/views.py
@@ -4,8 +4,24 @@ from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import Category
 
-from .serializers import WorkshopSerializer
+from .serializers import WorkshopSerializer, CategorySerializer
+
+
+class CategoryAPIView(APIView):
+    permission_classes = []
+    serializer_class = CategorySerializer
+
+    def get(self, request):
+        category = Category.objects.all()
+        if not category.exists():
+            return Response(
+                {"message": "No category available."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        serializer = CategorySerializer(category, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class WorkshopAPIView(APIView):
