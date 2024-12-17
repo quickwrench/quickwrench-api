36390cff194851f61020f4a222f050bb1fcec822
Youssef <Y.Ehab2289@nu.edu.eg>
2024-12-17 23:53:49 +0200

    test: modify users conftest,py after local mege conflict

--- tests/apps/users/conftest.py
+++ tests/apps/users/conftest.py
@@ -43,3 +43,15 @@ def user_with_existing_email(db, existing_user, load_data):
         car_make=car_make,
     )
     return user
+
+
+@pytest.fixture()
+def user_to_fetch(db, load_carmake_data) -> User:
+    car_make: CarMake = CarMake.objects.get(id=1)
+    account: Account = Account.objects.create_user(
+        email="testuser@test.com", username="username123", password="testpass"
+    )
+    user: User = User.objects.create(
+        account=account, first_name="first", last_name="last", car_make=car_make
+    )
+    return user
