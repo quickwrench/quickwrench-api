32ed0d58e64ab5968b27424b817ad0b4ead1d2e1
Youssef <Y.Ehab2289@nu.edu.eg>
2024-12-20 18:43:31 +0200

    refactor: remove test_service fixture to the public conftest because it is used by more than an app in tests

--- tests/apps/workshops/conftest.py
+++ tests/apps/workshops/conftest.py
@@ -6,17 +6,6 @@ from quickwrench_api.apps.workshops.models import Category, Service, Workshop
 
 
 @pytest.fixture
-def test_service(db, load_data) -> Service:
-    category: Category = Category.objects.get(id=1)
-    return Service.objects.create(
-        category=category,
-        name="Oil",
-        description="description",
-        price=99,
-    )
-
-
-@pytest.fixture
 def workshop_data(test_account: dict[str, str], test_service: Service):
     return {
         "account": test_account,
